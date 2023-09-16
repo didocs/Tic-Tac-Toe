05c8044e280e9a879307aa7bd518ebd7c039ce2d
Daniel Bercea <128615941+didocs@users.noreply.github.com>
2023-09-16 16:42:34 +0300

    Update scriptTic-Tac-Toe.js

--- scriptTic-Tac-Toe.js
+++ scriptTic-Tac-Toe.js
@@ -1,11 +1,11 @@
-let rounds = 9,
-    currentPlayer = "O",
-    Xmoves = [],
-    Omoves = [],
-    winnerCells = [],
-    playedCell = [],
-    grid = document.querySelectorAll(".cell"),
-    winnerLine = [
+let rounds = 9;
+let currentPlayer = "O";
+let xMoves = [];
+let oMoves = [];
+let winnerCells = [];
+let playedCell = [];
+let grid = document.querySelectorAll(".cell");
+let winnerLine = [
         [1, 2, 3],
         [4, 5, 6],
         [7, 8, 9],
@@ -16,12 +16,14 @@ let rounds = 9,
         [3, 5, 7]
     ];
 
-grid.forEach(function(cell) {
-    cell.addEventListener("click", function() {
-        const id = cell.id;
-        playGame(id);
+function startGame() {
+    grid.forEach(function(cell) {
+        cell.addEventListener("click", function() {
+            const id = cell.id;
+            playGame(id);
+        });
     });
-});
+}
 
 function playGame(id) {
     let cell = document.getElementById(id);
@@ -29,11 +31,15 @@ function playGame(id) {
         --rounds;
         currentPlayer = currentPlayer === "X" ? "O" : "X";
         cell.innerText = currentPlayer;
-        currentPlayer === "X" ? Xmoves.push(Number(id)) : Omoves.push(Number(id));
+         if (currentPlayer === "X") {
+            xMoves.push(Number(id));
+        } else { 
+            oMoves.push(Number(id));
+        }
         rounds === 0 ? gameOver(2) : null;
     }
-    checkWinnerLine(Xmoves) ? gameOver(0) : null;
-    checkWinnerLine(Omoves) ? gameOver(1) : null;
+    checkWinnerLine(xMoves) ? gameOver(0) : null;
+    checkWinnerLine(oMoves) ? gameOver(1) : null;
     playedCell.includes(id) ? true : playedCell.push(id);
 }
 
@@ -53,8 +59,7 @@ function gameOver(index) {
     let textBox = document.getElementById('messageBox');
     textBox.innerText = text[index];
     if (winnerCells.length > 0) {
-        grid.forEach(cell => winnerCells.includes(Number(cell.id)) ? 
-        cell.style.backgroundColor = "lightgreen" : null);
+        grid.forEach(cell => winnerCells.includes(Number(cell.id)) ? cell.style.backgroundColor = "lightgreen" : null);
     }
 }
 
